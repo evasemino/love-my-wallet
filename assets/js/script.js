53547f0034da3f65742a58ab474eb21435cdeae5
Eva <semino.eva@gmail.com>
2020-04-17 19:37:50 -0300

    ultimos cambios antes de la correccion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,17 @@
 $(document).ready(function () {
-    $('.pago__primary-button')
-        // Reciba un 'click'
+    $('.btn btn-secondary agregar')
         .on(
             'click',
-            // Ejecutar la siguiente función
             function () {
-                // Aquí no hace falta usar el e.preventDefault, porque es un botón que al
-                // hacerle click no tiene un comportamiento por defecto, salvo que sea un
-                // botón con type="submit"
-
-                // Guardo en una variable el elemento a clonar
                 var Duplicarpago =
-                    <li class="list-group-item">
-                        <article class="d-flex justify-content-between p-2 bd-highlight">
-                            <div class="text-uppercase tax-name">ECOGAS</div>
-                            <div class="tax-price">$592</div>
-                            <div class="tax-action">
-                                <a href="#" class="btn btn-primary text-uppercase">pagar</a>
-                            </div>
-                        </article>
-                    </li>
+                    <div class="pago__container-primary row">
+                        <p class="col-6 pago__description">Fibertel</p>
+                        <button type="button" class="col-6 btn btn-secondary pagar">Pagar</button>
+                    </div>
+
                     ;
-                $('.pago__primary-button').append(Duplicarpago);
+                $('.btn btn-secondary agregar').append(Duplicarpago);
             },
         );
-    // Fin función Clonar Elemento
 });
-  // Fin función DocumentReady
+
