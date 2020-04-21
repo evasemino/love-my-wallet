955d946d194c965221782c0c492b688cf267ebea
Eva <semino.eva@gmail.com>
2020-04-20 22:01:00 -0300

    mensaje de confirmaversion avanzada entrega final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,35 @@
 $(document).ready(function () {
-    $('.btn btn-secondary agregar')
+
+    $('.agregar-servicio')
+
         .on(
             'click',
-            function () {
-                var Duplicarpago =
-                    <div class="pago__container-primary row">
-                        <p class="col-6 pago__description">Fibertel</p>
-                        <button type="button" class="col-6 btn btn-secondary pagar">Pagar</button>
-                    </div>
 
-                    ;
-                $('.btn btn-secondary agregar').append(Duplicarpago);
+            function () {
+
+                var lementoAPagar = `
+                <br> 
+
+                <li class="list-group-item cuenta__container py-3 my-3">
+                <article class="servicio d-flex justify-content-between p-2 bd-highlight">
+                <div>
+                <h2 class="text-uppercase servicio__nombre">Afip</h2>
+                <h4 class="servicio__precio">Monto: $2500</h4>
+                <h4 class="servicio__vencimiento">Vencimiento: 20/05/20 </h4>
+            </div>
+            <div class="tax-action">
+                  <a href="pago.html" class="btn btn-primary agregar text-uppercase">pagar</a>
+                </div>
+              </article>
+                   
+                
+            </li> 
+          `;
+                $('.servicios-impagos').append(lementoAPagar);
             },
         );
+
 });
+
+
 
